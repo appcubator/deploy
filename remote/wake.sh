4f36dba1f2a22f26911acb19480b02ad13f03a4c
Ubuntu <ubuntu@ip-10-204-203-78.ec2.internal>
2014-04-15 04:52:54 +0000

    scripts for doing crazy remote things

---
+++ remote/wake.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# USAGE ./wake.sh <host> <deploy_id>
+
+set -e
+
+if [ -z $1 ]; then
+    echo "Please provide host as the first argument"
+    exit 1
+fi
+if [ -z $2 ]; then
+    echo "Please provide deployment id as the second argument"
+    exit 1
+fi
+
+HOST=$1
+DEPID=$2
+
+echo "
+bash container/wake.sh $i
+bash container/prox.sh $i
+" | ssh -i pk.pem deployer@"$HOST" 'cd deploy && bash -s'
