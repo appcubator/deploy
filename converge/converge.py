4ac9d31e8ded52672af3be4cfb9b485ae6ecbbf9
Ubuntu <ubuntu@ip-10-204-203-78.ec2.internal>
2014-04-23 04:02:42 +0000

    plan

--- converge/converge.py
+++ converge/converge.py
@@ -1,4 +1,14 @@
 # input the files
-# get the docker state of the remote hosts
+#   list of container, host pairs
+
+# for each remote host:
+#   remote get list of containers
+
 # diff the target state with the current state to output a list of actions
+#   to_create = target_containers - remote_containers
+#   to_delete = remote_containers - target_containers
+
 # execute the actions using ssh
+#   for each remote host:
+#     call remote bulk_create shell script, pass to_create containers to stdin.
+#     call remote bulk_delete shell script, pass to_delete containers to stdin.
