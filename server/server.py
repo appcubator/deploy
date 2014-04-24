0fe01c0d9149291996ff0a0793a7ffe120bcf9c9
Karan Sikka <kssworld93@gmail.com>
2014-04-23 23:20:52 -0400

    boom

--- server/server.py
+++ server/server.py
@@ -3,6 +3,11 @@
 See http://pointlessprogramming.wordpress.com/2011/02/13/python-cgi-tutorial-1/
 """
 
+# allows this script to be run from anywhere
+import os
+import sys
+os.chdir(os.path.dirname(sys.argv[0]))
+
 import BaseHTTPServer
 import CGIHTTPServer
 import cgitb; cgitb.enable()  ## This line enables CGI error reporting
@@ -12,7 +17,7 @@ server_address = ("", 8000)
 server = BaseHTTPServer.HTTPServer
 handler = CGIHTTPServer.CGIHTTPRequestHandler
 
-handler.cgi_directories = ["/cgi-bin"]
+handler.cgi_directories = ["/deployment"]
 
 httpd = server(server_address, handler)
 httpd.serve_forever()
