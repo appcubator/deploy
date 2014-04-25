f4ff72647f94ba05613b7b75de6cc0d6ee90c2ee
Karan Sikka <kssworld93@gmail.com>
2014-04-25 02:04:30 -0400

    stuff is working

---
+++ server.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from flask import Flask, request
+app = Flask(__name__)
+
+import re
+import json
+from controller.models import Machine, Container
+
+@app.route('/')
+def home():
+    return 'Yolo'
+
+@app.route('/deployment/list', methods=['GET'])
+def get_deployments():
+    username = request.args.get('username', '')
+    if username == '':
+        return 'plz pass username as a query string arg.', 400
+
+    username = username.lower()
+
+    if not re.match(r'^[0-9a-z]+$', username):
+        return 'invalid username, it must be alphanumeric only for now.', 400
+
+    ms = Machine.load_state(key=username)
+    containers = Container.load_state(ms, key=username)
+
+    return json.dumps(containers.keys())
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
